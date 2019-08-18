561e9e536c135522a7668b3edb94e51d6ea16a4f
Glenn A Doherty <gadz@earthlink.net>
2019-08-18 18:56:27 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,17 +20,17 @@ def third_challenge
       :patriarch => {
         :name => "Lord Capulet",
         :age => "50"
-        },
+      },
       :matriarch => {
         :name => "Lady Capulet"
         :age => "51"
-        },
+      },
       :heroine => {
         :name => "Juliet"
         :age => "15"
         :status => "alive"
-        },
+      },
       :heroine_friends => []
-   }
+    }
   }
 end
